e3f4f91351a769ac3092308e95dac335ae15b6e6
eignatik <evg.ignatik@gmail.com>
2017-04-21 22:14:38 +0300

    Todo comment has been deleted from calculator class (test for CI)

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
+
         return "";
     }
 
